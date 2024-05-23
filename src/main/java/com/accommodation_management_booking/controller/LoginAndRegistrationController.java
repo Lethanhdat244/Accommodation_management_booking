d0442c4da95ffb204f336f486ed611327d01d15f
ThanhDat <datlthe176015@fpt.edu.vn>
2024-05-24 03:11:18 +0700

    test cloudinary

---
+++ src/main/java/com/accommodation_management_booking/controller/LoginAndRegistrationController.java
@@ -0,0 +1,45 @@
+package com.accommodation_management_booking.controller;
+
+import com.accommodation_management_booking.dto.UserDTO;
+import com.accommodation_management_booking.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+public class LoginAndRegistrationController {
+
+    private UserService userService;
+
+    @GetMapping("register")
+    public String registerUser(Model model) {
+        UserDTO user = new UserDTO();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@ModelAttribute("user") UserDTO userDTO, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            // Nếu có lỗi validation, quay lại trang đăng ký
+            System.out.println("Error reddddd");
+            return "register";
+        }
+        try {
+            userService.saveUser(userDTO);
+        } catch (Exception e) {
+            // Nếu có lỗi trong quá trình lưu, quay lại trang đăng ký với thông báo lỗi
+            model.addAttribute("errorMessage", "There was an error registering the user. Please try again.");
+            return "register";
+        }
+        return "redirect:/register?success";
+    }
+
+}
