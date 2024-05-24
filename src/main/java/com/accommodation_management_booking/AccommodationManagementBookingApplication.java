6a1d11ae0cd19c686072f93f2a5d4236da49a092
Tran Thanh Binh <binhtthe171542@fpt.edu.vn>
2024-05-24 21:32:23 +0700

    Revert "xoa"
    This reverts commit 910a774047da7ead11cbdadc257474aecf7005aa.

---
+++ src/main/java/com/accommodation_management_booking/AccommodationManagementBookingApplication.java
@@ -0,0 +1,13 @@
+package com.accommodation_management_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationManagementBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationManagementBookingApplication.class, args);
+	}
+
+}
