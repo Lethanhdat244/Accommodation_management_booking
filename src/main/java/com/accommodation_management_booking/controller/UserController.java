b9bbab71e3eec80f04c583d06836dc30f24ce291
Huy Nguyễn <thanhhuyk31h@gmail.com>
2024-05-27 14:49:54 +0700

    commit 27/5

--- src/main/java/com/accommodation_management_booking/controller/UserController.java
+++ src/main/java/com/accommodation_management_booking/controller/UserController.java
@@ -1,21 +1,34 @@
 package com.accommodation_management_booking.controller;
 
-import ch.qos.logback.core.model.Model;
+import com.accommodation_management_booking.dto.UserDTO;
 import com.accommodation_management_booking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class UserController {
-    private UserService userService;
-    @GetMapping("/profile/view")
-    public String UserProfile(Model model){
 
-        return "userProfileView";
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile/view")
+    public String UserProfile(Model model) {
+        // Mã để hiển thị trang người dùng
+        return "userProfileView";  // Tên của template view (userProfileView.html)
     }
-    @GetMapping ("/change-password")
-    public String ChangePassword(Model model){
-        return "changePassword";
+
+    @PostMapping("/find")
+    public String get(@RequestParam(name = "id") int id, Model model) {
+        try {
+            UserDTO user = userService.getUser(id);
+            model.addAttribute("user", user);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", "User not found or an error occurred while retrieving the user.");
+        }
+        return "userProfileView";
     }
 }
