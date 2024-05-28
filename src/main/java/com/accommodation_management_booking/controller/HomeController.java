6bf6161c9f754496d7e4b97732bfb7082116c5fe
Pham Xuan Son <sonpxhe171548@fpt.edu.vn>
2024-05-28 13:30:54 +0700

    forgot password turn 2

--- src/main/java/com/accommodation_management_booking/controller/HomeController.java
+++ src/main/java/com/accommodation_management_booking/controller/HomeController.java
@@ -9,74 +9,9 @@ public class HomeController {
 
     @GetMapping("home")
     public String homepage(){
-        return "index";
-    }
-
-    @GetMapping("admin/home")
-    public String adminHomepage(){
-        return "admin/index";
+        return "homepage";
     }
 
 
-    @GetMapping("contact")
-    public String contact(){
-        return "contact";
-    }
-
-    @GetMapping("about")
-    public String about(){
-        return "about";
-    }
-
-    @GetMapping("user-management")
-    public String userManagement(){
-        return "user-management";
-    }
-
-    @GetMapping("student-management")
-    public String studentManagement(){
-        return "student-management";
-    }
-
-
-    @GetMapping("profile")
-    public String profile(){
-        return "profile";
-    }
-
-    @GetMapping("index")
-    public String index(){
-        return "index";
-    }
-
-    @GetMapping("blog")
-    public String blog(){
-        return "blog";
-    }
-
-    @GetMapping("buy")
-    public String buy(){
-        return "profile";
-    }
-
-    @GetMapping("properties")
-    public String properties(){
-        return "properties";
-    }
-
-    @GetMapping("property_details")
-    public String propertyDetails(){
-        return "property_details";
-    }
-
-    @GetMapping("rent")
-    public String rent(){
-        return "rent";
-    }
-
-    @GetMapping("view-list")
-    public String viewList(){
-        return "view-list";
-    }
 
 }
