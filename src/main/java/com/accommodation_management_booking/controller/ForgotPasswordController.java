0e59b7582a8bdd8b76852349b1a63b78ec90ac0c
Pham Xuan Son <sonpxhe171548@fpt.edu.vn>
2024-05-28 13:15:24 +0700

    forgot password, send email

---
+++ src/main/java/com/accommodation_management_booking/controller/ForgotPasswordController.java
@@ -0,0 +1,33 @@
+package com.accommodation_management_booking.controller;
+
+import com.accommodation_management_booking.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+public class ForgotPasswordController {
+
+    private final UserService userService;
+
+    @GetMapping("/forgot-password")
+    public String showForgotPasswordForm() {
+        return "forgot_password";
+    }
+
+    @PostMapping("/forgot-password")
+    public String processForgotPassword(@RequestParam("email") String email, Model model) {
+        try {
+            userService.processForgotPassword(email);
+            model.addAttribute("message", "A reset password link has been sent to your email.");
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "forgot_password";
+        }
+        return "redirect:/forgot-password?success";
+    }
+}
