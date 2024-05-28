cabc49943fe23cbdff3ede1aeea135a673d1b48f
Pham Xuan Son <sonpxhe171548@fpt.edu.vn>
2024-05-28 13:00:25 +0700

    reset

--- src/main/java/com/accommodation_management_booking/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.accommodation_management_booking.service;
-
-public interface EmailService {
-    void sendPasswordResetEmail(String email, String resetLink);
-    void sendRegistrationEmail(String email, String content);
-    void sendCompleteRegistrationEmail(String email, String content);
-}
