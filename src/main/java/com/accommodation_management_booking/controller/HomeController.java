4f502ded6f701f25aa3f0a32ffbae272f3e7d233
Pham Xuan Son <sonpxhe171548@fpt.edu.vn>
2024-05-30 00:21:22 +0700

    admin homepage

--- src/main/java/com/accommodation_management_booking/controller/HomeController.java
+++ src/main/java/com/accommodation_management_booking/controller/HomeController.java
@@ -40,4 +40,39 @@ public class HomeController {
     public String contact(){
         return "contact";
     }
+
+    @GetMapping("admin_homepage")
+    public String admin_homepage(){
+        return "admin_homepage";
+    }
+
+    @GetMapping("admin_list_student")
+    public String admin_list_student(){
+        return "admin_list_student";
+    }
+
+    @GetMapping("admin_add_student")
+    public String admin_add_student(){
+        return "admin_add_student";
+    }
+
+    @GetMapping("admin_list_room")
+    public String admin_list_room(){
+        return "admin_list_room";
+    }
+
+    @GetMapping("admin_payment_list")
+    public String admin_payment_list(){
+        return "admin_payment_list";
+    }
+
+    @GetMapping("admin_add_new_type_room")
+    public String admin_add_new_type_room(){
+        return "admin_add_new_type_room";
+    }
+
+    @GetMapping("admin_add_new_room")
+    public String admin_add_new_room(){
+        return "admin_add_new_room";
+    }
 }
