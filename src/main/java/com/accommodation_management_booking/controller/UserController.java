2db5ea63eddd05925609581c8c1e3e7705ef5693
Huy Nguyễn <thanhhuyk31h@gmail.com>
2024-05-30 21:55:17 +0700

    commit 30/5

--- src/main/java/com/accommodation_management_booking/controller/UserController.java
+++ src/main/java/com/accommodation_management_booking/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,10 +16,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/profile")
-    public String UserProfile(Model model) {
+    @GetMapping("/profile/{id}")
+    public String UserProfile(@PathVariable(name = "id") int id, Model model) {
         try {
-            UserDTO user = userService.getUser(1);
+            UserDTO user = userService.getUser(id);
             model.addAttribute("user", user);
         } catch (Exception ex) {
             model.addAttribute("errorMessage", "User not found or an error occurred while retrieving the user.");
@@ -30,7 +31,7 @@ public class UserController {
         model.addAttribute("user_id", id);
         return "changePasswordRequest";
     }
-    @GetMapping("/change-password")
+    @PostMapping("/change-password")
     public String ChangePassword(@RequestParam(name = "user_id")int id,
                                  @RequestParam(name = "password") String password,
                                  @RequestParam(name = "NewPassword") String NewPassword,
