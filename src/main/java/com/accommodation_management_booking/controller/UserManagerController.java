eb8e0f8e8da782ef52db047d476fe3c20f64db36
Tran Thanh Binh <binhtthe171542@fpt.edu.vn>
2024-05-30 14:02:43 +0700

    Update usermanager

--- src/main/java/com/accommodation_management_booking/controller/UserManagerController.java
+++ src/main/java/com/accommodation_management_booking/controller/UserManagerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,27 +27,60 @@ public class UserManagerController {
         return "user-manager/index";
     }
 
+    @GetMapping({"/search"})
+    public String searchUserList(Model model, @RequestParam(value = "keyword", required = false) String keyword,
+                                 @RequestParam(value = "category", required = false) String category) {
+        List<User> users = new ArrayList<>();
+        if (keyword == null || keyword.isEmpty()) {
+            users = repo.findAll();
+        } else if (category.isEmpty()) {
+            users = repo.searchAllBy(keyword);
+        } else {
+            switch (category) {
+                case "ID":
+                    try {
+                        int id = Integer.parseInt(keyword);
+                        users.add(repo.findById(id).orElse(null));
+                    } catch (NumberFormatException e) {
+                        users = null;
+                    }
+                    break;
+                case "Name": users = repo.searchByName(keyword); break;
+                case "Email": users = repo.searchByEmail(keyword); break;
+                case "Phone": users = repo.searchByPhoneNumber(keyword); break;
+            }
+        }
+        model.addAttribute("users", users);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("selectedCategory", category);
+        return "user-manager/index";
+    }
+
     @GetMapping("/edit")
     public String showEditPage (Model model, @RequestParam int id) {
         try {
-            User user = repo.findById(id).get();
-            model.addAttribute("user", user);
+            if (repo.findById(id).isPresent()) {
+                User user = repo.findById(id).get();
 
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(user.getUsername());
-            userDTO.setEmail(user.getEmail());
-            userDTO.setPhoneNumber(user.getPhoneNumber());
-            userDTO.setGender(user.getGender());
-            userDTO.setAddress(user.getAddress());
-            userDTO.setBirthdate(user.getBirthdate());
-            userDTO.setRoleUser(user.getRoleUser());
-            userDTO.setActive(user.isActive());
-            userDTO.setCccdNumber(user.getCccdNumber());
-            model.addAttribute("userDTO", userDTO);
+                model.addAttribute("user", user);
+
+                UserDTO userDTO = new UserDTO();
+                userDTO.setUsername(user.getUsername());
+                userDTO.setEmail(user.getEmail());
+                userDTO.setPhoneNumber(user.getPhoneNumber());
+                userDTO.setGender(user.getGender());
+                userDTO.setAddress(user.getAddress());
+                userDTO.setBirthdate(user.getBirthdate());
+                userDTO.setRoleUser(user.getRoleUser());
+                userDTO.setCccdNumber(user.getCccdNumber());
+                model.addAttribute("userDTO", userDTO);
+            } else throw new Exception();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
             return "redirect:/user-manager";
         }
         return "user-manager/edit-user";
     }
+
+
 }
--- src/main/java/com/accommodation_management_booking/repository/UserRepository.java
+++ src/main/java/com/accommodation_management_booking/repository/UserRepository.java
@@ -1,9 +1,23 @@
 package com.accommodation_management_booking.repository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.accommodation_management_booking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String username);
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE (u.username LIKE %:keyword% OR u.email LIKE %:keyword% OR u.phoneNumber LIKE %:keyword%)")
+    List<User> searchAllBy(@Param("keyword") String keyword);
+
+    @Query("SELECT u FROM User u WHERE (u.username LIKE %:keyword%)")
+    List<User> searchByName(@Param("keyword") String keyword);
+
+    @Query("SELECT u FROM User u WHERE (u.email LIKE %:keyword%)")
+    List<User> searchByEmail(@Param("keyword") String keyword);
+
+    @Query("SELECT u FROM User u WHERE (u.phoneNumber LIKE %:keyword%)")
+    List<User> searchByPhoneNumber(@Param("keyword") String keyword);
 }
