bb3ddb11ed5423021d8089756ae5008101b33a0f
Huy Nguyễn <thanhhuyk31h@gmail.com>
2024-06-03 22:49:11 +0700

    commit employee home, request, complain

--- src/main/java/com/accommodation_management_booking/controller/EmployeeController.java
+++ src/main/java/com/accommodation_management_booking/controller/EmployeeController.java
@@ -1,6 +1,12 @@
 package com.accommodation_management_booking.controller;
 
+import com.accommodation_management_booking.dto.UserDTO;
+import com.accommodation_management_booking.entity.User;
+import com.accommodation_management_booking.repository.UserRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,8 +15,13 @@ public class EmployeeController {
     public String employee_homepage(){
         return "employee_homepage";
     }
-    @GetMapping("fpt-dorm/employee/student")
-    public String employee_student(){return "employee_student";}
+    @GetMapping("fpt-dorm/employee/students-list")
+    public String employee_student(@AuthenticationPrincipal OAuth2User oauth2User,
+                                   @AuthenticationPrincipal UserDTO user,
+                                   Model model)
+    {
+        return "employee_student";
+    }
     @GetMapping("fpt-dorm/employee/complain")
     public String employee_complain(){return "employee_complain";}
 }
--- src/main/java/com/accommodation_management_booking/controller/RequestController.java
+++ src/main/java/com/accommodation_management_booking/controller/RequestController.java
@@ -1,12 +1,44 @@
 package com.accommodation_management_booking.controller;
 
+import com.accommodation_management_booking.dto.ComplaintDTO;
+import com.accommodation_management_booking.dto.UserDTO;
+import com.accommodation_management_booking.entity.Complaint;
+import com.accommodation_management_booking.entity.User;
+import com.accommodation_management_booking.repository.ComplainRepository;
+import com.accommodation_management_booking.repository.UserRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.Console;
+import java.util.List;
 
 @Controller
 public class RequestController {
+    UserRepository userRepository;
+    ComplainRepository complainRepository;
     @GetMapping("fpt-dorm/home-user/my-request")
-    public String studentRequest() {
+    public String studentRequest(@AuthenticationPrincipal OAuth2User oauth2User,
+                                 @AuthenticationPrincipal UserDTO user,
+                                 Model model) {
+        String email = null;
+
+        if (oauth2User != null) {
+            email = oauth2User.getAttribute("email");
+        } else if (user != null) {
+            email = user.getEmail();
+        }
+        User onlineUser;
+        if (email != null) {
+            onlineUser = userRepository.findByEmail(email);
+        }
+        else {
+            return "redirect:/fpt-dorm/login";
+        }
+        List<Complaint> complaints = complainRepository.findByUserUserId(onlineUser.getUserId());
+        model.addAttribute("complainList", complaints);
         return "student_request";
     }
 }
