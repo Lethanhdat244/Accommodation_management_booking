ca2499d15310a54b0fb0427378879a394e9ea377
Huy Nguyễn <thanhhuyk31h@gmail.com>
2024-06-04 12:00:52 +0700

    commit ComplainRepository, student_faq.html Edit employee_complain.html, EmployeeController.java, student_request.html, student_usage_services.html

---
+++ src/main/java/com/accommodation_management_booking/repository/ComplainRepository.java
@@ -0,0 +1,15 @@
+package com.accommodation_management_booking.repository;
+
+import com.accommodation_management_booking.dto.ComplaintDTO;
+import com.accommodation_management_booking.entity.Complaint;
+import com.accommodation_management_booking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ComplainRepository extends JpaRepository<Complaint, Integer> {
+    @Query("SELECT c FROM Complaint c")
+    List<Complaint> getAllRequest();
+}
