1d654316dabe7eafa7b307a1ad92594b66a793a5
Huy Nguyễn <thanhhuyk31h@gmail.com>
2024-06-04 12:05:10 +0700

    commit ComplainRepository, student_faq.html Edit employee_complain.html, EmployeeController.java, student_request.html, student_usage_services.html

--- src/main/java/com/accommodation_management_booking/controller/UsageServiceController.java
+++ src/main/java/com/accommodation_management_booking/controller/UsageServiceController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UsedServiceController {
+public class UsageServiceController {
     @GetMapping("/fpt-dorm/home-user/usage-service")
     public String fptDormUsedServices() {
         return "student_usage_services";
