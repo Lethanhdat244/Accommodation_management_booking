0754196e95fcca2d66d0c671750b879cb136494a
Pham Xuan Son <sonpxhe171548@fpt.edu.vn>
2024-06-10 22:42:01 +0700

    employee profile

--- src/main/java/com/accommodation_management_booking/controller/EmployeeController.java
+++ src/main/java/com/accommodation_management_booking/controller/EmployeeController.java
@@ -6,7 +6,10 @@ import com.accommodation_management_booking.entity.User;
 import com.accommodation_management_booking.repository.ComplainRepository;
 import com.accommodation_management_booking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +22,19 @@ public class EmployeeController {
     @Autowired
     ComplainRepository complainRepository;
     @GetMapping("fpt-dorm/employee/home")
-    public String employee_homepage(){
+    public String admin_homepage(Model model, Authentication authentication){
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) authentication;
+            OAuth2User oauth2User = oauth2Token.getPrincipal();
+            String email = oauth2User.getAttribute("email");
+            model.addAttribute("email", email);
+        } else if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            model.addAttribute("email", userDetails.getUsername());
+        } else {
+            // Handle cases where the authentication is not OAuth2
+            model.addAttribute("email", "Unknown");
+        }
         return "employee_homepage";
     }
     @GetMapping("fpt-dorm/employee/students-list")
